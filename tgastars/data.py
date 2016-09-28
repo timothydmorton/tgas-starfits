b54eebc5b1360f0d1acd1f4d4b0718b20ee5f331
Timothy Morton <tim.morton@gmail.com>
2016-09-28 15:50:55 -0400

    generate completed list function/script

--- tgastars/data.py
+++ tgastars/data.py
@@ -1,8 +1,10 @@
 import pandas as pd
-import os
+import numpy as np
+import os, sys, glob
 
 GAIADIR = os.getenv('GAIADATA', os.path.expanduser('~/.gaia'))
 DATADIR = os.getenv('TGASTARS', os.path.expanduser('~/.tgastars'))
+STARMODELDIR = os.path.join(DATADIR, 'starmodels')
 
 try:
     TGAS = pd.read_hdf(os.path.join(GAIADIR, 'TgasSource.h5'), 'df')
@@ -22,3 +24,25 @@ def dirname(i):
     gid = source_id(i)
 
     return os.path.join(DATADIR, 'starmodels', str(gid)[:3], str(gid))
+
+def update_completed():
+    all_stars = []
+    dirs = os.listdir(STARMODELDIR)
+
+    for d in dirs:
+        d = os.path.join(STARMODELDIR, d)
+        if not os.path.isdir(d):
+            continue
+        ini_files = glob.glob('{}/*/star.ini'.format(d))
+        all_stars = all_stars + [os.path.basename(os.path.dirname(f))
+                                 for f in ini_files]
+
+    all_stars = np.array(all_stars)
+    np.random.sort(all_stars)
+    np.savetxt(os.path.join(DATADIR, 'completed.list'), all_stars, fmt='%s')
+
+
+def get_completed():
+    """
+    returns list of stellar IDs that have been fit
+    """
