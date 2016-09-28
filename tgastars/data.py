2230a2196a22444876bbbc17f11587563d71af5e
Timothy Morton <tim.morton@gmail.com>
2016-09-28 17:04:46 -0400

    reading ids

--- tgastars/data.py
+++ tgastars/data.py
@@ -70,10 +70,10 @@ def get_completed_ids():
     """
     returns list of stellar IDs that have been fit
     """
-    return np.loadtxt(os.path.join(DATADIR, 'completed.list'), fmt='%i')
+    return np.loadtxt(os.path.join(DATADIR, 'completed.list'), dtype=int)
 
 def get_ready_ids():
     """
     returns list of stellar IDs that have been fit
     """
-    return np.loadtxt(os.path.join(DATADIR, 'ready.list'), fmt='%i')
+    return np.loadtxt(os.path.join(DATADIR, 'ready.list'), dtype=int)
