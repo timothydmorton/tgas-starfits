ff7d5acedeb321b481d060142f2fd007e8d7a6af
Timothy Morton <tim.morton@gmail.com>
2016-09-29 12:42:38 -0400

    cleaned up

--- tgastars/models.py
+++ tgastars/models.py
@@ -9,18 +9,20 @@ from .data import dirname
 
 class GaiaDR1_StarModel(StarModel):
 
-    def _corrected_parallax(self, val, unc):
-        """ Returns new value and uncertainty for parallax, given systematic correction
+    @property
+    def corrected_parallax(self):
+        if not hasattr(self, '_corrected_parallax'):
+            d = {}
+            for s, (val, unc) in self.obs.parallax.items():
+                if val < 1.:
+                    d[s] = val, sqrt(unc**2 + 0.3**2)
+                else:            
+                    offset = -0.08 - 0.27*log10(val)
+                    d[s] = val - offset, sqrt(unc**2 + 0.3**2)
+            self._corrected_parallax = d
 
-        According to http://arxiv.org/abs/1609.05390
-        """
-        # For small parallaxes, no offest, just systematic error term
-        if val < 1.:
-            return val, sqrt(unc**2 + 0.3**2)
-        else:            
-            offset = -0.08 - 0.27*log10(val)
-            return val - offset, sqrt(unc**2 + 0.3**2)
-
+        return self._corrected_parallax
+    
     def lnlike(p, **kwargs):
         lnl = super(GaiaDR1_StarModel, self).lnlike(p, **kwargs)
 
@@ -31,11 +33,15 @@ class GaiaDR1_StarModel(StarModel):
         for s,(val,err) in self.obs.parallax.items():
             dist = pardict['{}_0'.format(s)][3]
             mod = 1./dist * 1000.
-            lnl += 0.5*(val-mod)**2/err**2 #undoing base StarModel parallax lnl
+            
+            # Undo base StarModel parallax lnl term
+            lnl += 0.5*(val-mod)**2/err**2 
 
-            val, err = self._corrected_parallax(val, err)
+            # Redo with corrected values
+            val, err = self.corrected_parallax[s]
             lnl += -0.5*(val-mod)**2/err**2
 
+        return lnl
 
 
 def get_starmodel(i, modelname='dartmouth_starmodel_single'):
